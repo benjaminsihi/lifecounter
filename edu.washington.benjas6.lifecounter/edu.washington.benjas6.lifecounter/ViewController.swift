7833ef1e5d1e11c9ddbb0bbc371e53a32f0185ea
benjaminshi <benjaminshi97@yahoo.com>
2019-01-29 18:47:51 -0800

    initialize the project

---
+++ edu.washington.benjas6.lifecounter/edu.washington.benjas6.lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  edu.washington.benjas6.lifecounter
+//
+//  Created by Benjamin Shi on 1/29/19.
+//  Copyright © 2019 Benjamin Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
